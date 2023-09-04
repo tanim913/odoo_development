f6ea897f80982949a622f0f8499b921d12e130cc
Azizullah Tanim <42853370+tanim913@users.noreply.github.com>
2023-09-04 22:49:53 +0600

    [ADD] new arrivals

--- theme_custom_web/__manifest__.py
+++ theme_custom_web/__manifest__.py
@@ -4,11 +4,15 @@
     'category': 'Theme',
     'sequence': 10,
     'version': '1.0',
-    'depends': ['website'],
+    'depends': ['website','stock'],
     'data': [
+        'security/ir.model.access.csv',
         'views/header.xml',
         'views/footer.xml',
         'views/homepage.xml',
+        'views/new_arrivals_views.xml',
+        'views/snippets/jewelry_making.xml',
+        'views/snippets/snippets.xml',
     ],
     'images': [
     ],
@@ -16,7 +20,8 @@
     },
 'assets':{
         'web.assets_frontend': [
-            'theme_custom_web/static/src/scss/styles.scss'
+            'theme_custom_web/static/src/scss/styles.scss',
+            'theme_custom_web/static/src/scss/making_jewelry.scss'
         ],
         'web._assets_primary_variables': [
             "theme_custom_web/static/src/scss/primary_variables.scss",
